ed73b352662610f0c61f5af14993f79101c4a610
gabvteixeira <gabriel.vteixeira@mercadolivre.com>
2022-11-24 11:12:30 -0300

    Aula de Variaveis e Tipos de Dados:

---
+++ Aula2-Variaveis/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variavel1 string = "variável 1"
+	fmt.Println(variavel1)
+
+	variavel2 := "variavel 2"
+	fmt.Println(variavel2)
+
+	variavel3, variavel4 := "variavel 3", "variavel 4"
+	fmt.Println(variavel3)
+	fmt.Println(variavel4)
+
+	const constante1 string = "constante1"
+	fmt.Println(constante1)
+
+}
